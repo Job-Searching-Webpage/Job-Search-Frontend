bd669196cc3115de79d7991340191cc97de5125c
Ledjo <ledjo.lleshaj@gmail.com>
2022-04-24 06:30:45 -0700

    Added simple nav Bar using tailwind styles

--- src/playground.js
+++ src/playground.js
@@ -1,7 +1,21 @@
-const fruits = ["Apple", "Banana", "Orange"];
+/*const fruits = ["Apple", "Banana", "Orange"];
 const vegetables = ["Cucumber", "Radish"];
 
 //console.log([fruits, vegetables]); //does not concatenate the arrays
-//copies the array data into a new array without saving the old array structure
-
+//copies the array data into a new array without saving old array structure
 console.log([...fruits, ...vegetables]);
+*/
+
+const developer = {
+  salary: 100000,
+  experience: 4.5,
+  techStack: ["Vue", "HTML", "CSS"],
+  lookingForWork: true,
+  doubleSalary() {
+    this.salary = this.salary * 2;
+    this.lookingForWork = false;
+  },
+};
+console.log(developer);
+developer.doubleSalary();
+console.log(developer);
