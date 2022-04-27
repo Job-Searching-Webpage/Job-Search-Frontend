9bec322c5a05bd5a8ea174890116677c6fff4010
Ledjo <ledjo.lleshaj@gmail.com>
2022-04-27 08:57:54 -0700

    Added the form

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,31 +1,29 @@
 <template>
   <main>
-    <section class="flex flex-col h-screen pt-10 pb-20 bg-yellow-200">
+    <section class="flex flex-col h-screen pt-10 pb-20">
       <div class="grid grid-cols-12">
-        <div class="col-start-1 col-span-1 border border-blue-700">
-          1 column
-        </div>
+        <div class="col-start-1 col-span-1"></div>
 
-        <div class="col-start-2 col-span-5 border border-blue-700">
+        <div class="col-start-2 col-span-5">
           <head-line />
+          <JobSearchForm />
         </div>
 
-        <div class="col-start-7 col-span-5 border border-blue-700">5column</div>
+        <div class="col-start-7 col-span-5"></div>
 
-        <div class="col-start-12 col-span-1 border border-blue-700">
-          1 column
-        </div>
+        <div class="col-start-12 col-span-1"></div>
       </div>
     </section>
   </main>
 </template>
 <script>
 import HeadLine from "@/components/HeadLine.vue";
-
+import JobSearchForm from "@/components/jobSearchForm.vue";
 export default {
   name: "Hero",
   components: {
     HeadLine,
+    JobSearchForm,
   },
 };
 </script>
