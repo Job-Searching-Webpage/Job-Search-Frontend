b7d2e674725f8c7ab9bb0dda314dba6f5b57e014
Ledjo <ledjo.lleshaj@gmail.com>
2022-05-04 02:22:43 -0700

    Added Vuex

--- src/main.js
+++ src/main.js
@@ -4,13 +4,15 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-import App from "./App.vue";
-import "./assets/tailwind.css";
-import router from "./router";
+import App from "@/App.vue";
+import "@/assets/tailwind.css";
+import router from "@/router";
+import store from "@/store";
 
 library.add(faSearch);
 
 createApp(App)
+  .use(store)
   .use(router)
   .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
