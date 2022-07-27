462147f29154c99fa637bf17c8d80315bd04c8b1
Ledjo <ledjo.lleshaj@gmail.com>
2022-07-27 06:07:13 -0700

    Fnished API Testing

--- tests/unit/store/index.test.js
+++ tests/unit/store/index.test.js
@@ -1,9 +1,16 @@
-import { state, mutations } from "@/store";
+import { state, mutations, actions } from "@/store";
+import getJobs from "@/api/getJobs";
+jest.mock("@/api/getJobs");
 
 describe("state", () => {
   it("keeps track of whether user is logged in", () => {
     const startingState = state();
     expect(startingState.isLoggedIn).toBe(false);
+  });
+
+  it("Stores job listings", () => {
+    const startingState = state();
+    expect(startingState.jobs).toEqual([]);
   });
 });
 
@@ -13,6 +20,50 @@ describe("mutations", () => {
       const state = { isLoggedIn: false };
       mutations.LOGIN_USER(state);
       expect(state).toEqual({ isLoggedIn: true });
+    });
+  });
+
+  describe("RECEIVE_JOBS", () => {
+    it("receives jobs from API response", () => {
+      const state = { jobs: [] };
+      mutations.RECEIVE_JOBS(state, [
+        {
+          id: 1,
+          title: "Java Engineer",
+        },
+      ]);
+      expect(state).toEqual({ jobs: [{ id: 1, title: "Java Engineer" }] });
+    });
+  });
+});
+
+describe("actions", () => {
+  describe("FETCH_JOBS", () => {
+    beforeEach(() => {
+      getJobs.mockResolvedValue([
+        {
+          id: 1,
+          title: "Software Developer",
+        },
+      ]);
+    });
+
+    it("makes request to fetch jobs", async () => {
+      const context = { commit: jest.fn() };
+      await actions.FETCH_JOBS({ commit: context.commit });
+      expect(getJobs).toHaveBeenCalled();
+    });
+
+    it("send message to save received jobs in store", async () => {
+      const commit = jest.fn();
+      const context = { commit };
+      await actions.FETCH_JOBS({ commit: context.commit });
+      expect(commit).toHaveBeenCalledWith("RECEIVE_JOBS", [
+        {
+          id: 1,
+          title: "Software Developer",
+        },
+      ]);
     });
   });
 });
