5d8043d2ac83476a34a61c29c9181af7c7a826b8
Ledjo <ledjo.lleshaj@gmail.com>
2022-07-27 09:03:41 -0700

    Added Accordion

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -2,11 +2,50 @@
   <div
     class="flex flex-col p-4 bg-white border-r border-solid border-brand-gray-1 w-96"
   >
-    Job Filters Sidebar
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="my-4 text-base font-semibold">What do u want to do?</h3>
+        <div class="flex items-center text-sm">
+          <action-button text="Clear filters" type="secondary" />
+        </div>
+      </div>
+      <accordion header="Job Types"> </accordion>
+
+      <accordion header="Organization">
+        <div class="mt-5">
+          <fieldset>
+            <ul class="flex flex-wrap flex-row">
+              <li class="w-1/2 h-8">
+                <input id="VueTube" type="checkbox" class="mr-3" />
+                <label for="VueTube">VueTube</label>
+              </li>
+              <li class="w-1/2 h-8">
+                <input id="Between Vue and Me" type="checkbox" class="mr-3" />
+                <label for="Between Vue and Me">Between Vue</label>
+              </li>
+              <li class="w-1/2 h-8">
+                <input id="Et Vue Brute" type="checkbox" class="mr-3" />
+                <label for="Et Vue Brute">Et Vue Brute</label>
+              </li>
+              <li class="w-1/2 h-8">
+                <input id="Vue and a Half Men" type="checkbox" class="mr-3" />
+                <label for="Vue and a Half Men">Vue and a Half Men</label>
+              </li>
+            </ul>
+          </fieldset>
+        </div>
+      </accordion>
+    </section>
   </div>
 </template>
 <script>
+import ActionButton from "@/components/Shared/ActionButton.vue";
+import Accordion from "@/components/Shared/Accordion.vue";
 export default {
   name: "JobFiltersSidebar",
+  components: {
+    ActionButton,
+    Accordion,
+  },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -2,14 +2,19 @@ import { createApp } from "vue";
 
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAngleDown,
+  faAngleUp,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
 
 import App from "@/App.vue";
 import "@/assets/tailwind.css";
 import router from "@/router";
 import store from "@/store";
 
-library.add(faSearch);
+library.add(faSearch, faAngleDown, faAngleUp);
+//library.add(faAngleDown);
 
 createApp(App)
   .use(store)
