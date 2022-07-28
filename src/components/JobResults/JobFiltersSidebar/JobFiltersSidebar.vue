1803c6030ce46651e6699258e63ca23cd3b0dae1
Ledjo <ledjo.lleshaj@gmail.com>
2022-07-28 02:20:10 -0700

    Added Slots to improve reusability and flexibility

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -12,42 +12,21 @@
       <accordion header="Job Types">
         <p>Placeholder</p>
       </accordion>
-
-      <accordion header="Organization">
-        <div class="mt-5">
-          <fieldset>
-            <ul class="flex flex-wrap flex-row">
-              <li class="w-1/2 h-8">
-                <input id="VueTube" type="checkbox" class="mr-3" />
-                <label for="VueTube">VueTube</label>
-              </li>
-              <li class="w-1/2 h-8">
-                <input id="Between Vue and Me" type="checkbox" class="mr-3" />
-                <label for="Between Vue and Me">Between Vue</label>
-              </li>
-              <li class="w-1/2 h-8">
-                <input id="Et Vue Brute" type="checkbox" class="mr-3" />
-                <label for="Et Vue Brute">Et Vue Brute</label>
-              </li>
-              <li class="w-1/2 h-8">
-                <input id="Vue and a Half Men" type="checkbox" class="mr-3" />
-                <label for="Vue and a Half Men">Vue and a Half Men</label>
-              </li>
-            </ul>
-          </fieldset>
-        </div>
-      </accordion>
+      <job-filters-sidebar-organizations></job-filters-sidebar-organizations>
     </section>
   </div>
 </template>
 <script>
 import ActionButton from "@/components/Shared/ActionButton.vue";
 import Accordion from "@/components/Shared/Accordion.vue";
+import JobFiltersSidebarOrganizations from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue";
+
 export default {
   name: "JobFiltersSidebar",
   components: {
     ActionButton,
     Accordion,
+    JobFiltersSidebarOrganizations,
   },
 };
 </script>
