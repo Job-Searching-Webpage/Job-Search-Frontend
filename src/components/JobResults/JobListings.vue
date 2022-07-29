1cc618b9cc0411e470edeeec9905c74e73c061e7
Ledjo <ledjo.lleshaj@gmail.com>
2022-07-29 07:02:26 -0700

    Bug Fixes

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -45,8 +45,8 @@
 </template>
 
 <script>
-import { mapActions, mapState } from "vuex";
-import { FETCH_JOBS } from "@/store";
+import { mapActions, mapGetters } from "vuex";
+import { FETCH_JOBS, FILTERED_JOBS_BY_ORGANIZATIONS } from "@/store";
 
 import JobListing from "@/components/JobResults/JobListing.vue";
 
@@ -57,6 +57,7 @@ export default {
   },
 
   computed: {
+    ...mapGetters([FILTERED_JOBS_BY_ORGANIZATIONS]),
     currentPage() {
       const pageString = this.$route.query.page || "1";
       return Number.parseInt(pageString);
@@ -68,16 +69,21 @@ export default {
     },
     nextPage() {
       const nextPage = this.currentPage + 1;
-      const maxPage = Math.ceil(this.jobs.length / 10);
+      const maxPage = Math.ceil(
+        this.FILTERED_JOBS_BY_ORGANIZATIONS.length / 10
+      );
       return nextPage <= maxPage ? nextPage : undefined;
     },
     displayedJobs() {
       const pageNumber = this.currentPage;
       const firstJobIndex = (pageNumber - 1) * 10;
       const lastJobIndex = pageNumber * 10;
-      return this.jobs.slice(firstJobIndex, lastJobIndex);
+      return this.FILTERED_JOBS_BY_ORGANIZATIONS.slice(
+        firstJobIndex,
+        lastJobIndex
+      );
     },
-    ...mapState(["jobs"]),
+    //...mapState(["jobs"]),
   },
   async mounted() {
     //this.$store.dispatch(FETCH_JOBS);
