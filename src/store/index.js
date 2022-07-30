49f8a621959c6bd46232c781e5b70552bc9d18df
Ledjo <ledjo.lleshaj@gmail.com>
2022-07-30 07:05:58 -0700

    Fixed broken imports

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
 import { createStore } from "vuex";
-import state from "@store/state";
-import getters from "@store/getters";
-import mutations from "@store/mutations";
-import actions from "@store/actions";
+import state from "@/store/state";
+import getters from "@/store/getters";
+import mutations from "@/store/mutations";
+import actions from "@/store/actions";
 
 const store = createStore({
   state,
