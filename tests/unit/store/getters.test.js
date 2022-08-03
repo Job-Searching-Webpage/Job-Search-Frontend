98e3bd583512b6c1d6db1beb6f4dd7a68c72ac0b
Ledjo <ledjo.lleshaj@gmail.com>
2022-08-03 07:02:18 -0700

    New tests for getter functions

--- tests/unit/store/getters.test.js
+++ tests/unit/store/getters.test.js
@@ -106,4 +106,45 @@ describe("getters", () => {
       });
     });
   });
+
+  describe("INCLUDE_JOB_BY_ORGANIZATION", () => {
+    describe("When user has not selected any organizations", () => {
+      it("includes jobs", () => {
+        const state = {
+          selectedOrganizations: [],
+        };
+        const job = { organization: "Google" };
+        const includeJob = getters.INCLUDE_JOB_BY_ORGANIZATION(state)(job);
+        expect(includeJob).toBe(true);
+      });
+    });
+    it("identifies if job is associated by the given organization", () => {
+      const state = {
+        selectedOrganizations: ["Google", "Microsoft"],
+      };
+      const job = { organization: "Google" };
+      const includeJob = getters.INCLUDE_JOB_BY_ORGANIZATION(state)(job);
+      expect(includeJob).toBe(true);
+    });
+  });
+  describe("INCLUDE_JOB_BY_JOB_TYPE", () => {
+    describe("When user has not selected any job types", () => {
+      it("includes jobs", () => {
+        const state = {
+          selectedJobTypes: [],
+        };
+        const job = { jobType: "Full-time" };
+        const includeJob = getters.INCLUDE_JOB_BY_JOB_TYPE(state)(job);
+        expect(includeJob).toBe(true);
+      });
+    });
+    it("identifies if job is associated by the given job type", () => {
+      const state = {
+        selectedJobTypes: ["Full-time", "Part-time"],
+      };
+      const job = { jobType: "Full-time" };
+      const includeJob = getters.INCLUDE_JOB_BY_JOB_TYPE(state)(job);
+      expect(includeJob).toBe(true);
+    });
+  });
 });
