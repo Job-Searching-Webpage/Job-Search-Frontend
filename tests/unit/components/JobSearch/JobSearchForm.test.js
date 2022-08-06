2fb590ae05c02d761a86228ce65443f8f6dfa8d0
Ledjo <ledjo.lleshaj@gmail.com>
2022-08-06 05:39:33 -0700

    Fixed Failing tests

--- tests/unit/components/JobSearch/JobSearchForm.test.js
+++ tests/unit/components/JobSearch/JobSearchForm.test.js
@@ -1,18 +1,18 @@
 import { mount } from "@vue/test-utils";
+import { useRouter } from "vue-router";
+jest.mock("vue-router");
 import JobSearchForm from "@/components/JobSearch/jobSearchForm";
 
 describe("JobsSearchForm", () => {
   describe("when user submits form", () => {
     it("directs user to job results page with user's search parameters", async () => {
       const push = jest.fn();
-      const $router = { push };
-
+      useRouter.mockReturnValue({
+        push,
+      });
       const wrapper = mount(JobSearchForm, {
         attachTo: document.body,
         global: {
-          mocks: {
-            $router,
-          },
           stubs: {
             FontAwesomeIcon: true,
           },
