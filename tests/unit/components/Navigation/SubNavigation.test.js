718c2ba2161d623735ff825d0e98e0c559f059a2
Ledjo <ledjo.lleshaj@gmail.com>
2022-08-06 06:26:40 -0700

    Fixed Failing Tests from Refactoring

--- tests/unit/components/Navigation/SubNavigation.test.js
+++ tests/unit/components/Navigation/SubNavigation.test.js
@@ -1,16 +1,15 @@
 import { mount } from "@vue/test-utils";
 
+import { useRoute } from "vue-router";
+import { useStore } from "vuex";
+jest.mock("vue-router");
+jest.mock("vuex");
+
 import SubNavigation from "@/components/Navigation/SubNavigation";
 
 describe("SubNavigation bar", () => {
-  const createConfig = (routeName, $store = {}) => ({
+  const createConfig = () => ({
     global: {
-      mocks: {
-        $route: {
-          name: routeName,
-        },
-        $store,
-      },
       stubs: {
         FontAwesomeIcon: true,
       },
@@ -18,14 +17,17 @@ describe("SubNavigation bar", () => {
   });
   describe("when user is on job page", () => {
     it("displays job count", () => {
-      const routeName = "JobResults";
-      const $store = {
+      useRoute.mockReturnValue({
+        name: "JobResults",
+      });
+
+      useStore.mockReturnValue({
         getters: {
           FILTERED_JOBS: [{ id: 1 }, { id: 2 }],
         },
-      };
+      });
 
-      const wrapper = mount(SubNavigation, createConfig(routeName, $store));
+      const wrapper = mount(SubNavigation, createConfig());
       const jobCount = wrapper.find("[data-test='job-count']");
       expect(jobCount.text()).toMatch("2 jobs matched");
     });
@@ -33,9 +35,10 @@ describe("SubNavigation bar", () => {
 
   describe("when user is not on job page", () => {
     it("does NOT display job count", () => {
-      const routeName = "Home";
-
-      const wrapper = mount(SubNavigation, createConfig(routeName));
+      useRoute.mockReturnValue({
+        name: "Home",
+      });
+      const wrapper = mount(SubNavigation, createConfig());
       const jobCount = wrapper.find("[data-test='job-count']");
       expect(jobCount.exists()).toBe(false);
     });
