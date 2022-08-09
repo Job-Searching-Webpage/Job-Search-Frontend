802f3f5f84c3f9f123bd4819a7da756039d61cd9
Ledjo <ledjo.lleshaj@gmail.com>
2022-08-09 07:58:16 -0700

    Composables for uniques organizations +tested

--- src/store/composables.js
+++ src/store/composables.js
@@ -1,7 +1,11 @@
 import { computed } from "vue";
 import { useStore } from "vuex";
 
-import { FILTERED_JOBS, UNIQUE_JOB_TYPES } from "@/store/constants";
+import {
+  FILTERED_JOBS,
+  UNIQUE_JOB_TYPES,
+  UNIQUE_ORGANIZATIONS,
+} from "@/store/constants";
 
 export const useFilteredJobs = () => {
   const store = useStore();
@@ -11,4 +15,9 @@ export const useFilteredJobs = () => {
 export const useUniqueJobTypes = () => {
   const store = useStore();
   return computed(() => store.getters[UNIQUE_JOB_TYPES]);
+};
+
+export const useUniqueOrganizations = () => {
+  const store = useStore();
+  return computed(() => store.getters[UNIQUE_ORGANIZATIONS]);
 };
--- tests/unit/store/composables.test.js
+++ tests/unit/store/composables.test.js
@@ -3,6 +3,7 @@ jest.mock("vuex");
 
 import { useFilteredJobs } from "@/store/composables";
 import { useUniqueJobTypes } from "@/store/composables";
+import { useUniqueOrganizations } from "@/store/composables";
 
 describe("composables", () => {
   describe("useFilteredJobs", () => {
@@ -16,18 +17,30 @@ describe("composables", () => {
       const result = useFilteredJobs();
       expect(result.value).toEqual([{ id: 1 }]);
     });
-
-    describe("useUniqueJobTypes", () => {
-      it("retrieves unique job types from the store", () => {
-        useStore.mockReturnValue({
-          getters: {
-            UNIQUE_JOB_TYPES: new Set(["Full-time"]),
-          },
-        });
-
-        const result = useUniqueJobTypes();
-        expect(result.value).toEqual(new Set(["Full-time"]));
+  });
+  describe("useUniqueJobTypes", () => {
+    it("retrieves unique job types from the store", () => {
+      useStore.mockReturnValue({
+        getters: {
+          UNIQUE_JOB_TYPES: new Set(["Full-time"]),
+        },
       });
+
+      const result = useUniqueJobTypes();
+      expect(result.value).toEqual(new Set(["Full-time"]));
+    });
+  });
+
+  describe("useUniqueOrganizations", () => {
+    it("retrieves unique organizations from the store", () => {
+      useStore.mockReturnValue({
+        getters: {
+          UNIQUE_ORGANIZATIONS: new Set(["Apple"]),
+        },
+      });
+
+      const result = useUniqueOrganizations();
+      expect(result.value).toEqual(new Set(["Apple"]));
     });
   });
 });
