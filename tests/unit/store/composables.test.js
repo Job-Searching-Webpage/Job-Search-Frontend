a6714722a8c8dffd522ea0bd38b1f86b4175ba97
Ledjo <ledjo.lleshaj@gmail.com>
2022-08-09 07:53:42 -0700

    Tests for unique job types composable

--- tests/unit/store/composables.test.js
+++ tests/unit/store/composables.test.js
@@ -2,6 +2,7 @@ import { useStore } from "vuex";
 jest.mock("vuex");
 
 import { useFilteredJobs } from "@/store/composables";
+import { useUniqueJobTypes } from "@/store/composables";
 
 describe("composables", () => {
   describe("useFilteredJobs", () => {
@@ -14,6 +15,19 @@ describe("composables", () => {
 
       const result = useFilteredJobs();
       expect(result.value).toEqual([{ id: 1 }]);
+    });
+
+    describe("useUniqueJobTypes", () => {
+      it("retrieves unique job types from the store", () => {
+        useStore.mockReturnValue({
+          getters: {
+            UNIQUE_JOB_TYPES: new Set(["Full-time"]),
+          },
+        });
+
+        const result = useUniqueJobTypes();
+        expect(result.value).toEqual(new Set(["Full-time"]));
+      });
     });
   });
 });
