04dcbd29f97e33295f9ce3bebc798f386a369cc9
Ledjo <ledjo.lleshaj@gmail.com>
2022-08-12 06:20:52 -0700

    2

--- tests/unit/store/getters.test.ts
+++ tests/unit/store/getters.test.ts
@@ -1,15 +1,15 @@
 import getters from "@/store/getters";
+import { createState, createJob } from "./utils";
 
 describe("getters", () => {
   describe("UNIQUE_ORGANIZATIONS", () => {
     it("find unique organizations from list of jobs", () => {
-      const state = {
-        jobs: [
-          { organization: "Google" },
-          { organization: "Amazon" },
-          { organization: "Google" },
-        ],
-      };
+      const jobs = [
+        createJob({ organization: "Google" }),
+        createJob({ organization: "Amazon" }),
+        createJob({ organization: "Google" }),
+      ];
+      const state = createState({ jobs });
       const result = getters.UNIQUE_ORGANIZATIONS(state);
       expect(result).toEqual(new Set(["Google", "Amazon"]));
     });
@@ -17,13 +17,12 @@ describe("getters", () => {
 
   describe("UNIQUE_JOBS_TYPES", () => {
     it("find unique job types from list of jobs", () => {
-      const state = {
-        jobs: [
-          { jobType: "Full-time" },
-          { jobType: "Temporary" },
-          { jobType: "Full-time" },
-        ],
-      };
+      const jobs = [
+        createJob({ jobType: "Full-time" }),
+        createJob({ jobType: "Temporary" }),
+        createJob({ jobType: "Full-time" }),
+      ];
+      const state = createState({ jobs });
       const result = getters.UNIQUE_JOB_TYPES(state);
       expect(result).toEqual(new Set(["Full-time", "Temporary"]));
     });
@@ -32,19 +31,17 @@ describe("getters", () => {
   describe("INCLUDE_JOB_BY_ORGANIZATION", () => {
     describe("When user has not selected any organizations", () => {
       it("includes jobs", () => {
-        const state = {
-          selectedOrganizations: [],
-        };
-        const job = { organization: "Google" };
+        const state = createState({ selectedOrganizations: [] });
+        const job = createJob({ organization: "Google" });
         const includeJob = getters.INCLUDE_JOB_BY_ORGANIZATION(state)(job);
         expect(includeJob).toBe(true);
       });
     });
     it("identifies if job is associated by the given organization", () => {
-      const state = {
+      const state = createState({
         selectedOrganizations: ["Google", "Microsoft"],
-      };
-      const job = { organization: "Google" };
+      });
+      const job = createJob({ organization: "Google" });
       const includeJob = getters.INCLUDE_JOB_BY_ORGANIZATION(state)(job);
       expect(includeJob).toBe(true);
     });
@@ -52,19 +49,17 @@ describe("getters", () => {
   describe("INCLUDE_JOB_BY_JOB_TYPE", () => {
     describe("When user has not selected any job types", () => {
       it("includes jobs", () => {
-        const state = {
-          selectedJobTypes: [],
-        };
-        const job = { jobType: "Full-time" };
+        const state = createState({ selectedJobTypes: [] });
+        const job = createJob({ jobType: "Full-time" });
         const includeJob = getters.INCLUDE_JOB_BY_JOB_TYPE(state)(job);
         expect(includeJob).toBe(true);
       });
     });
     it("identifies if job is associated by the given job type", () => {
-      const state = {
+      const state = createState({
         selectedJobTypes: ["Full-time", "Part-time"],
-      };
-      const job = { jobType: "Part-time" };
+      });
+      const job = createJob({ jobType: "Full-time" });
       const includeJob = getters.INCLUDE_JOB_BY_JOB_TYPE(state)(job);
       expect(includeJob).toBe(true);
     });
@@ -78,10 +73,10 @@ describe("getters", () => {
         INCLUDE_JOB_BY_ORGANIZATION,
         INCLUDE_JOB_BY_JOB_TYPE,
       };
-      const job = { id: 1, title: "Best job ever" };
-      const state = {
+      const job = createJob({ title: "Best job ever" });
+      const state = createState({
         jobs: [job],
-      };
+      });
 
       const result = getters.FILTERED_JOBS(state, mockGetters);
       expect(result).toEqual([job]);
