97f19b36f46e83e56b0bb3312e640c53ede2db50
Ledjo <ledjo.lleshaj@gmail.com>
2022-08-16 05:51:12 -0700

    Added get degree actions and tested

--- tests/unit/store/actions.test.ts
+++ tests/unit/store/actions.test.ts
@@ -5,6 +5,10 @@ jest.mock("@/api/getJobs");
 
 const getJobMock = getJobs as jest.Mock;
 
+import getDegrees from "@/api/getDegrees";
+jest.mock("@/api/getDegrees");
+const getDegreeMock = getDegrees as jest.Mock;
+
 describe("actions", () => {
   describe("FETCH_JOBS", () => {
     beforeEach(() => {
@@ -18,7 +22,7 @@ describe("actions", () => {
 
     it("makes request to fetch jobs", async () => {
       const context = { commit: jest.fn() };
-      await actions.FETCH_JOBS({ commit: context.commit });
+      await actions.FETCH_JOBS(context);
       expect(getJobs).toHaveBeenCalled();
     });
 
@@ -30,6 +34,35 @@ describe("actions", () => {
         {
           id: 1,
           title: "Software Developer",
+        },
+      ]);
+    });
+  });
+
+  describe("FETCH_DEGREES", () => {
+    beforeEach(() => {
+      getDegreeMock.mockResolvedValue([
+        {
+          id: 1,
+          degree: "Bachelor'",
+        },
+      ]);
+    });
+
+    it("makes request to fetch degree", async () => {
+      const context = { commit: jest.fn() };
+      await actions.FETCH_DEGREES({ commit: context.commit });
+      expect(getDegrees).toHaveBeenCalled();
+    });
+
+    it("send message to save received degree in store", async () => {
+      const commit = jest.fn();
+      const context = { commit };
+      await actions.FETCH_DEGREES(context);
+      expect(commit).toHaveBeenCalledWith("RECEIVE_DEGREES", [
+        {
+          id: 1,
+          degree: "Bachelor'",
         },
       ]);
     });
