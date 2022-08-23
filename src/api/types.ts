d9188646a4594a6311b0dfa8e35b21891b19deca
Ledjo <ledjo.lleshaj@gmail.com>
2022-08-23 15:35:59 -0700

    Added Team type

--- src/api/types.ts
+++ src/api/types.ts
@@ -15,3 +15,38 @@ export interface Degree {
   id: number;
   degree: string;
 }
+
+export interface Team {
+  CF: string;
+  name: string;
+  cognome: string;
+  dataNascita: Date;
+  birthplace: string;
+  nazionalita: string;
+  address: string;
+  jobType: string;
+  disponibilita: string;
+  phone: string;
+  email: string;
+  languages: string[];
+  patenta: string;
+  macchina: boolean;
+  qualification: string;
+  esperienze: experience[];
+  backUpPerson: BackUpPerson;
+}
+
+export interface experience {
+  azienda: string;
+  periodo: string;
+  duties: string;
+  luogo: string;
+  payment: string;
+}
+
+export interface BackUpPerson {
+  name: string;
+  cognome: string;
+  email: string;
+  phone: string;
+}
