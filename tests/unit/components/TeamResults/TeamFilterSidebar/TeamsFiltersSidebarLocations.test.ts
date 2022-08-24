6d039b1bbecd7570efbc4f1468ffb76716b851ce
Ledjo <ledjo.lleshaj@gmail.com>
2022-08-24 15:41:41 -0700

    Tested teams filter about Location

---
+++ tests/unit/components/TeamResults/TeamFilterSidebar/TeamsFiltersSidebarLocations.test.ts
@@ -0,0 +1,20 @@
+import { shallowMount } from "@vue/test-utils";
+import { useUniqueLocations } from "@/store/composables";
+jest.mock("@/store/composables");
+
+const useUniqueLocationsMock = useUniqueLocations as jest.Mock;
+
+import TeamFiltersSidebarLocations from "@/components/TeamResults/TeamFiltersSidebar/TeamFiltersSidebarLocations.vue";
+
+describe("TeamFilterSidebarCar", () => {
+  it("allows user to filter workers by car disponibility", () => {
+    useUniqueLocationsMock.mockReturnValue(new Set(["Earth", "Planet"]));
+    const wrapper = shallowMount(TeamFiltersSidebarLocations);
+    const carOwningFilter = wrapper.findComponent({
+      name: "TeamFiltersSidebarCheckboxGroup",
+    });
+    const { uniqueValues, mutation } = carOwningFilter.props();
+    expect(uniqueValues).toEqual(new Set(["Earth", "Planet"]));
+    expect(mutation).toBe("ADD_SELECTED_LOCATIONS");
+  });
+});
