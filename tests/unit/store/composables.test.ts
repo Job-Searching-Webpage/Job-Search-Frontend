2f0926d6cbe66f2ce4f1802528b27f7821724bd2
Ledjo <ledjo.lleshaj@gmail.com>
2022-08-24 07:54:53 -0700

    ADDED new tests for team functions

--- tests/unit/store/composables.test.ts
+++ tests/unit/store/composables.test.ts
@@ -3,11 +3,19 @@ jest.mock("vuex");
 
 import {
   useFilteredJobs,
+  useFilteredTeams,
   useUniqueJobTypes,
   useUniqueOrganizations,
+  useUniqueCar,
+  useUniqueLocations,
+  useUniqueQualifications,
+  useUniqueTeamLanguages,
+  useUniquePatenta,
+  useUniquePeriods,
   useFetchJobsDispatch,
   useUniqueDegrees,
   useFetchDegreesDispatch,
+  useFetchTeamsDispatch,
 } from "@/store/composables";
 
 const useStoreMock = useStore as jest.Mock;
@@ -22,6 +30,19 @@ describe("composables", () => {
       });
 
       const result = useFilteredJobs();
+      expect(result.value).toEqual([{ id: 1 }]);
+    });
+  });
+
+  describe("useFilteredTeams", () => {
+    it("retrieves filtered teams from store", () => {
+      useStoreMock.mockReturnValue({
+        getters: {
+          FILTERED_TEAMS: [{ id: 1 }],
+        },
+      });
+
+      const result = useFilteredTeams();
       expect(result.value).toEqual([{ id: 1 }]);
     });
   });
@@ -65,6 +86,84 @@ describe("composables", () => {
     });
   });
 
+  describe("useUniqueLocations", () => {
+    it("retrieves unique locations from store", () => {
+      useStoreMock.mockReturnValue({
+        getters: {
+          UNIQUE_LOCATIONS: new Set(["San Francisco"]),
+        },
+      });
+
+      const result = useUniqueLocations();
+      expect(result.value).toEqual(new Set(["San Francisco"]));
+    });
+  });
+
+  describe("useUniqueQualifications", () => {
+    it("retrieves unique qualifications from store", () => {
+      useStoreMock.mockReturnValue({
+        getters: {
+          UNIQUE_QUALIFICATIONS: new Set(["Bachelor's degree"]),
+        },
+      });
+
+      const result = useUniqueQualifications();
+      expect(result.value).toEqual(new Set(["Bachelor's degree"]));
+    });
+  });
+
+  describe("useUniqueLanguages", () => {
+    it("retrieves unique languages from store", () => {
+      useStoreMock.mockReturnValue({
+        getters: {
+          UNIQUE_LANGUAGES: new Set(["English"]),
+        },
+      });
+
+      const result = useUniqueTeamLanguages();
+      expect(result.value).toEqual(new Set(["English"]));
+    });
+  });
+
+  describe("useUniquePeriods", () => {
+    it("retrieves unique periods from store", () => {
+      useStoreMock.mockReturnValue({
+        getters: {
+          UNIQUE_PERIODS: new Set(["1 year"]),
+        },
+      });
+
+      const result = useUniquePeriods();
+      expect(result.value).toEqual(new Set(["1 year"]));
+    });
+  });
+
+  describe("useUniquePatenta", () => {
+    it("retrieves unique patenta from store", () => {
+      useStoreMock.mockReturnValue({
+        getters: {
+          UNIQUE_PATENTA: new Set(["True"]),
+        },
+      });
+
+      const result = useUniquePatenta();
+      expect(result.value).toEqual(new Set(["True"]));
+    });
+  });
+
+  describe("useUniqueCar", () => {
+    it("retrieves unique car from store", () => {
+      useStoreMock.mockReturnValue({
+        getters: {
+          UNIQUE_CAR: new Set(["True"]),
+        },
+      });
+
+      const result = useUniqueCar();
+      expect(result.value).toEqual(new Set(["True"]));
+    });
+  });
+
   describe("useFetchJobsDispatch", () => {
     it("sends call to fetch jobs from API", () => {
       const dispatch = jest.fn();
@@ -84,6 +183,17 @@ describe("composables", () => {
       });
       useFetchDegreesDispatch();
       expect(dispatch).toHaveBeenCalledWith("FETCH_DEGREES");
+    });
+  });
+
+  describe("useFetchTeamsDispatch", () => {
+    it("sends call to fetch teams from API", () => {
+      const dispatch = jest.fn();
+      useStoreMock.mockReturnValue({
+        dispatch,
+      });
+      useFetchTeamsDispatch();
+      expect(dispatch).toHaveBeenCalledWith("FETCH_TEAMS");
     });
   });
 });
