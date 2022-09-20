3d530c8fe6145df97339311627294005d3e4dc33
LedjoLleshaj <ledjo.lleshaj@gmail.com>
2022-09-21 00:18:00 +0200

    New apis for backup and experience

--- src/api/getBackupById.ts
+++ src/api/getBackupById.ts
@@ -8,7 +8,7 @@ export default async function getBackupById(
 
   try {
     const response = await axios.get<BackUpPerson>(
-      `${baseUrl}/getBackupById/${backupId}`
+      `${baseUrl}/getBackUpById/${backupId}`
     );
     return response.data;
   } catch (error) {
